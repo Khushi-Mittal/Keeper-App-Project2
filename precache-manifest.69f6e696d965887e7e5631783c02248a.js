7a6cbdf96f12bceb127a82036aad6776127cf2c5
Khushi-Mittal <khushimittal0587@gmail.com>
2022-07-02 19:58:04 +0530

    Updates

---
+++ precache-manifest.69f6e696d965887e7e5631783c02248a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e695a93152c91b302e916fc0feed339",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7f30ba515d4e6407363",
+    "url": "/static/js/2.82dff26f.chunk.js"
+  },
+  {
+    "revision": "db13ba44e3d2fd5ca6a0",
+    "url": "/static/js/main.df051a16.chunk.js"
+  },
+  {
+    "revision": "641f15582db5e9b7b4fe",
+    "url": "/static/js/runtime-main.2f3ca742.js"
+  }
+]);
